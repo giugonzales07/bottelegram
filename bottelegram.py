d75c95a3cc5cfc07c70b806472e7ca7ebac57d0a
Giuliano Gonzales <92641300+giugonzales07@users.noreply.github.com>
2025-03-16 14:20:54 -0300

    feat: Update bottelegram.py

--- bottelegram.py
+++ bottelegram.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 CHAV_API = "6581266371:AAHb_wLY10rLLXJwyq78lM_yUIg4CVW2IIU"
 
 bot = telebot.TeleBot(CHAV_API)
-ano= 2024       #formato AAAA
-mes=  1         #usar numero
+ano= 2025       #formato AAAA
+mes= 12         #usar numero
 dia= 31
 
 @bot.message_handler(commands=["contagem"])
@@ -19,8 +19,7 @@ def contagem(mensagem):
     elif datapadrao <= hoje:    
         delta = hoje - datapadrao
 
-    bot.send_message(mensagem.chat.id, "Olá, " + mensagem.from_user.first_name + "\nhoje é dia " + str(hoje.day) + "/" + str(hoje.month) + "\nfaltam: 83.333 para o VERDE !!!" + "\nfaltam: " + str(delta.days) + " dias pras auditorias do EDL !!!")
-    #bot.send_message(mensagem.chat.id, "Olá, " + mensagem.from_user.first_name + "\nhoje (" + str(hoje.day) + "/" + str(hoje.month)  + ") é um belo dia para a Operação Emproguetes\n\n" + "obs: não se esqueça, Somos Tri Alto Impacto!!!!!!!!!!!!!")
+    bot.send_message(mensagem.chat.id, "Olá, " + mensagem.from_user.first_name + "\nhoje é dia " + str(hoje.day) + "/" + str(hoje.month) + "\nfaltam: 10.000 para o VERDE !!!" + "\nfaltam: " + str(delta.days) + " dias pras auditorias do EDL !!!")
     
 def verificar(mensagem):
     return True
